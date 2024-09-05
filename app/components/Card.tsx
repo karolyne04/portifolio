88edfe65d4fbe8694466364c877f6cdc6bd2f956
karolyne04 <ferreiracarolyne04@gmail.com>
2024-09-04 22:32:27 -0300

    final 01

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -10,27 +10,28 @@ interface CardProps {
 
 export default function Card({ imageUrl, name, link, className }: CardProps) {
   return (
-    <div className="w-full h-full max-w-lg bg-teal-600 rounded-lg shadow-lg overflow-hidden transition-transform transform hover:scale-105">
-      <div className="relative w-full h-72">
-        <Image 
-          src={imageUrl} 
-          alt={`${name} image`} 
-          // fill
-          // className="object-cover"
-          layout="fill" objectFit="cover"
-          // sizes="(max-width: 640px) 100vw, (max-width: 768px) 50vw, (max-width: 1024px) 33vw, 25vw"
-        />
+    <Link href={link || "#"} passHref>
+      <div className="w-full h-full max-w-lg bg-white rounded-lg shadow-lg overflow-hidden transition-transform transform hover:scale-105">
+        <div className="relative w-full h-72">
+          <Image 
+            src={imageUrl} 
+            alt={`${name} image`} 
+            layout="fill" 
+            objectFit="cover"
+            className="object-cover"
+          />
+        </div>
+        <div className="px-6 py-4 bg-gray-100 h-32">
+          <div className="font-bold text-xl mb-2 text-primary">{name}</div>
+          {link ? (
+            <span className="text-blue-500 hover:underline">
+              {link}
+            </span>
+          ) : (
+            <p className="text-gray-500">Link não disponível</p>
+          )}
+        </div>
       </div>
-      <div className="px-6 py-4 bg-five h-32">
-        <div className="font-bold text-xl mb-2">{name}</div>
-        {link ? (
-          <Link href={link} className="text-white hover:underline">
-            {link}
-          </Link>
-        ) : (
-          <p className="text-gray-500">Link não disponível</p>
-        )}
-      </div>
-    </div>
+    </Link>
   );
 }
